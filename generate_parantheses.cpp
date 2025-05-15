fc39d2382de0c7e36cf8e29fa50a3878ea240473
redouane <nour1r3d1@gmail.com>
2025-05-15 17:42:18 +0100

    solve https://leetcode.com/problems/generate-parentheses

---
+++ generate_parantheses.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+class Solution {
+public:
+  std::vector<std::string> generateParenthesis(int n) {
+    std::vector<std::string> r;
+    std::string p;
+
+    this->generate(0, 0, n, p, r);
+
+    return r;
+  }
+
+private:
+  void generate(int o, int c, int n, std::string &p,
+                std::vector<std::string> &r) {
+    if (o == n && o == c) {
+      r.push_back(p);
+      return;
+    }
+
+    if (o < n) {
+      p.push_back('(');
+      this->generate(o + 1, c, n, p, r);
+      p.pop_back();
+    }
+
+    if (c < o) {
+      p.push_back(')');
+      this->generate(o, c + 1, n, p, r);
+      p.pop_back();
+    }
+  }
+};
