4b45c29febe77cb075587a18aa84ccbc12a9ccf4
redouane <nour1r3d1@gmail.com>
2025-05-18 05:53:29 +0100

    solve https://leetcode.com/problems/symmetric-tree

---
+++ symmetric_tree.cpp
@@ -0,0 +1,30 @@
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  bool isSymmetric(TreeNode *root) {
+    return isSymmetricHelper(root->left, root->right);
+  }
+
+private:
+  bool isSymmetricHelper(TreeNode *t1, TreeNode *t2) {
+    if (!t1 && !t2) {
+      return true;
+    }
+
+    if (!t1 || !t2) {
+      return false;
+    }
+
+    return t1->val == t2->val && isSymmetricHelper(t1->left, t2->right) &&
+           isSymmetricHelper(t1->right, t2->left);
+  }
+};
