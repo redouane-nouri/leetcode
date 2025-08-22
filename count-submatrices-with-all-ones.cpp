2251df866dca8d8306dc66d9d152f10842ef4de3
redouane <nour1r3d1@gmail.com>
2025-08-22 17:48:57 +0100

    re-solve https://leetcode.com/problems/count-submatrices-with-all-ones

--- count-submatrices-with-all-ones.cpp
+++ count-submatrices-with-all-ones.cpp
@@ -1,35 +1,23 @@
-#include <stack>
 #include <vector>
 
 class Solution {
 public:
   int numSubmat(std::vector<std::vector<int>> &mat) {
     int m = mat.size(), n = mat[0].size(), res = 0;
-    std::vector<int> h(n, 0);
 
     for (int i = 0; i < m; ++i) {
-      for (int j = 0; j < n; ++j)
-        if (mat[i][j] == 0)
-          h[j] = 0;
-        else
-          h[j]++;
-
-      std::stack<int> s;
-      std::vector<int> sum(n, 0);
-
       for (int j = 0; j < n; ++j) {
-        while (!s.empty() && h[s.top()] >= h[j])
-          s.pop();
+        if (mat[i][j]) {
+          if (j)
+            mat[i][j] += mat[i][j - 1];
 
-        if (!s.empty()) {
-          int top = s.top();
-          sum[j] = sum[top] + h[j] * (j - top);
-        } else {
-          sum[j] = h[j] * (j + 1);
+          for (int w = mat[i][j], k = i; k >= 0 && mat[k][j]; --k) {
+            if (mat[k][j] < w)
+              w = mat[k][j];
+
+            res += w;
+          }
         }
-
-        res += sum[j];
-        s.push(j);
       }
     }
 
