c86240740e817100cf4eae5bfc247a27737cb70d
redouane <nour1r3d1@gmail.com>
2025-08-30 17:58:43 +0100

    re-solve https://leetcode.com/problems/valid-sudoku

--- valid-sudoku.cpp
+++ valid-sudoku.cpp
@@ -1,47 +1,21 @@
-#include <unordered_set>
 #include <vector>
-
-#define CHECK_AND_INSERT                                                       \
-  if (c == '.')                                                                \
-    continue;                                                                  \
-  if (nums.count(c))                                                           \
-    return false;                                                              \
-  nums.insert(c);
 
 class Solution {
 public:
   bool isValidSudoku(std::vector<std::vector<char>> &board) {
-    std::unordered_set<char> nums;
-    char c;
-    int r, co;
+    int r[9] = {0}, c[9] = {0}, b[9] = {0};
 
     for (int i = 0; i < 9; ++i) {
-      nums.clear();
-
       for (int j = 0; j < 9; ++j) {
-        c = board[i][j];
+        if (board[i][j] != '.') {
+          int m = 1 << (board[i][j] - '0'), bi = i / 3 * 3 + j / 3;
 
-        CHECK_AND_INSERT
-      }
+          if ((r[i] & m) || (c[j] & m) || (b[bi] & m))
+            return false;
 
-      nums.clear();
-
-      for (int j = 0; j < 9; ++j) {
-        c = board[j][i];
-
-        CHECK_AND_INSERT
-      }
-
-      nums.clear();
-
-      r = (i / 3) * 3;
-      co = (i % 3) * 3;
-
-      for (int j = 0; j < 3; ++j) {
-        for (int k = 0; k < 3; ++k) {
-          c = board[r + j][co + k];
-
-          CHECK_AND_INSERT
+          r[i] |= m;
+          c[j] |= m;
+          b[bi] |= m;
         }
       }
     }
