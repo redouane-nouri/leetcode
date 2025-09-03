72952d84872ebe0e99eb1585e953e02cc36aa4ba
redouane <nour1r3d1@gmail.com>
2025-09-03 16:17:07 +0100

    resolve https://leetcode.com/problems/interleaving-string

--- interleaving-string.cpp
+++ interleaving-string.cpp
@@ -4,25 +4,24 @@
 class Solution {
 public:
   bool isInterleave(std::string s1, std::string s2, std::string s3) {
-    int s1n = s1.size(), s2n = s2.size();
+    int n = s1.size(), m = s2.size();
 
-    if (s1n + s2n != s3.size())
+    if (n + m != s3.size())
       return false;
 
-    std::vector<std::vector<bool>> dp(s1n + 1, std::vector<bool>(s2n + 1));
-    dp[0][0] = true;
+    std::vector<bool> d(m + 1);
+    d[0] = true;
 
-    for (int j = 1; j <= s2n; ++j)
-      dp[0][j] = dp[0][j - 1] && (s3[j - 1] == s2[j - 1]);
+    for (int j = 1; j <= m; ++j)
+      d[j] = d[j - 1] && (s3[j - 1] == s2[j - 1]);
 
-    for (int i = 1; i <= s1n; ++i)
-      dp[i][0] = dp[i - 1][0] && (s3[i - 1] == s1[i - 1]);
+    for (int i = 1; i <= n; ++i) {
+      d[0] = d[0] && (s1[i - 1] == s3[i - 1]);
+      for (int j = 1; j <= m; ++j)
+        d[j] = (d[j] && s1[i - 1] == s3[i + j - 1]) ||
+               (d[j - 1] && s2[j - 1] == s3[i + j - 1]);
+    }
 
-    for (int i = 1; i <= s1n; ++i)
-      for (int j = 1; j <= s2n; ++j)
-        dp[i][j] = (dp[i - 1][j] && (s3[i + j - 1] == s1[i - 1])) ||
-                   (dp[i][j - 1] && (s3[i + j - 1] == s2[j - 1]));
-
-    return dp[s1n][s2n];
+    return d[m];
   }
 };
