103ee9e44a56bb56e268ee6450e43a2bf1e8b590
redouane <nour1r3d1@gmail.com>
2025-09-04 14:39:23 +0100

    re-solve https://leetcode.com/problems/recover-binary-search-tree

--- recover-binary-search-tree.cpp
+++ recover-binary-search-tree.cpp
@@ -1,4 +1,5 @@
 #include <climits>
+
 struct TreeNode {
   int val;
   TreeNode *left;
@@ -36,3 +37,60 @@ public:
     f->val ^= s->val;
   }
 };
+
+/*
+//Morris traversal solution
+#include <climits>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  void recoverTree(TreeNode *root) {
+    TreeNode *f = nullptr, *s = nullptr, *p = nullptr, *c = root, *pr;
+
+    while (c) {
+      if (!c->left) {
+        if (p && p->val >= c->val) {
+          if (!f)
+            f = p;
+          s = c;
+        }
+        p = c;
+        c = c->right;
+      } else {
+        pr = c->left;
+        while (pr->right && pr->right != c)
+          pr = pr->right;
+
+        if (!pr->right) {
+          pr->right = c;
+          c = c->left;
+        } else {
+          pr->right = nullptr;
+
+          if (p->val >= c->val) {
+            if (!f)
+              f = p;
+            s = c;
+          }
+
+          p = c;
+          c = c->right;
+        }
+      }
+    }
+
+    f->val ^= s->val;
+    s->val ^= f->val;
+    f->val ^= s->val;
+  }
+};
+*/
