3dc730646b2d21692ce09c1e467482f5dd983102
Redouane Nouri <nour1r3d1@gmail.com>
2026-01-24 13:40:20 +0100

    solve https://leetcode.com/problems/find-peak-element

---
+++ find-peak-element.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  int findPeakElement(vector<int> &nums) {
+    int l = 0, r = nums.size() - 1;
+    while (l < r) {
+      int m = l + (r - l >> 1);
+      nums[m] < nums[m + 1] ? l = m + 1 : r = m;
+    }
+    return l;
+  }
+};
