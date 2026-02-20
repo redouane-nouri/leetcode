a5307cc41f5bcd23ca1ae63179fb75f8f8b998a9
Redouane Nouri <nour1r3d1@gmail.com>
2026-02-20 14:48:14 +0100

    resolve https://leetcode.com/problems/binary-watch

--- binary-watch.cpp
+++ binary-watch.cpp
@@ -1,19 +1,18 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 class Solution {
 public:
-  std::vector<std::string> readBinaryWatch(int turnedOn) {
-    std::vector<std::string> res;
+  vector<string> readBinaryWatch(int turnedOn) {
+    vector<string> ans;
 
-    for (int i = 0; i < 12; ++i) {
-      for (int j = 0; j < 60; ++j) {
+    for (int i = 0; i < 12; ++i)
+      for (int j = 0; j < 60; ++j)
         if (__builtin_popcount(i) + __builtin_popcount(j) == turnedOn)
-          res.push_back(std::to_string(i) + ":" + (j < 10 ? "0" : "") +
-                        std::to_string(j));
-      }
-    }
+          ans.push_back(to_string(i) + ":" + (j < 10 ? "0" : "") +
+                        to_string(j));
 
-    return res;
+    return ans;
   }
 };
