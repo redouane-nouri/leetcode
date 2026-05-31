2d3742bc371a78db6b12e9b219390a5e4d3792e7
Redouane Nouri <nour1r3d1@gmail.com>
2026-05-31 20:38:12 +0100

    solve https://leetcode.com/problems/minimum-element-after-replacement-with-digit-sum

---
+++ minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,27 @@
+/**
+ * @author Redouane Nouri
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int ans = INT_MAX;
+
+        for (int &num : nums) {
+            int dig = 0;
+
+            while (num) {
+                dig += num % 10;
+                num /= 10;
+            }
+
+            ans = min(ans, dig);
+        }
+
+        return ans;
+    }
+};
