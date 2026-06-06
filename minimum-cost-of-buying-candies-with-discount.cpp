bfa3d6fc60a4bf70ad21034cbfe703dca90dbc8a
Redouane Nouri <nour1r3d1@gmail.com>
2026-06-06 15:50:58 +0100

    solve https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount

---
+++ minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,19 @@
+/**
+ * @author Redouane Nouri
+ */
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+        
+        int ans = 0;
+        for (int i = 0; i < cost.size(); ++i)
+            if (i % 3 != 2)
+                ans += cost[i];
+
+        return ans;
+    }
+};
